4075b4c0a4d2e9635cae849d9dc53c0da7b398cc
Aethar <elliott.ashby88@gmail.com>
2022-10-13 12:08:56 +0100

    tow

---
+++ two/q2_3.py
@@ -0,0 +1,20 @@
+from math import cos, fabs, pi
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def g(x):
+    n = 1
+    total = term = cos(x)
+    while fabs(term) > (1.0e-7 * fabs(total) + 1.0e-13):
+        n += 2
+        term = cos(n * x) / (n * n)
+        total += term
+    return total
+
+
+x = np.arange(0, 4*pi, 0.01)
+y = [g(i) for i in x]
+plt.plot(x, y)
+plt.xticks(ticks=[pi, 2 * pi, 3 * pi, 4 * pi], labels=['$\pi$', '2$\pi$', '3$\pi$', '4$\pi$'])
+plt.savefig('test.png', dpi=300)
