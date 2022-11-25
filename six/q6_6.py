e0502d108225459d30bf823372af6544258c0859
Aethar <elliott.ashby88@gmail.com>
2022-11-25 17:00:14 +0000

    merging

--- six/q6_6.py
+++ six/q6_6.py
@@ -1,6 +1,9 @@
 from math import cos
+from math import pi
 import copy
+from numpy import arange
 import matplotlib.pyplot as plt
+import scipy
 
 
 def odestep(f, y, t, dt, w):
@@ -32,23 +35,43 @@ if __name__ == '__main__':
         data: list = []
         for w in [0.5, 1, 2]:
             t = 0
-            dt = 0.0015
             y = i
-            startt = copy.copy(y)
+            dt = 0.0015
+            startt = copy.copy(i)
             tf = 30.0
             nsteps = int(tf / dt)
             temp1 = []
             temp2 = []
-            for i in range(nsteps):
+            for x in range(nsteps):
                 y = odestep(f, y, t, dt, w)
-                t = (i + 1) * dt
+                t = (x + 1) * dt
                 temp1.append(y)
                 temp2.append(t)
             data.append([temp2, temp1])
-        plt.plot(data[0][0], data[0][1])
-        plt.plot(data[1][0], data[1][1])
-        plt.plot(data[2][0], data[2][1])
-        plt.xlabel('RC[s]')
-        plt.xlabel('Voltage[V]')
+        fig, (axs0, axs1, axs2) = plt.subplots(3, 1, sharex=True, sharey=True)
+        axs0.plot(data[0][0], data[0][1])
+        axs0.set_title('$\omega$ = 0.5')
+        axs0.plot(arange(0, int(tf), 0.1), [f(0, t, 0.5) for t in arange(0, int(tf), 0.1)])
+        axs1.plot(data[1][0], data[1][1])
+        axs1.set_title('$\omega$ = 1')
+        axs1.plot(arange(0, int(tf), 0.1), [f(0, t, 1) for t in arange(0, int(tf), 0.1)])
+        axs2.plot(data[2][0], data[2][1])
+        axs2.set_title('$\omega$ = 2')
+        axs2.plot(arange(0, int(tf), 0.1), [f(0, t, 2) for t in arange(0, int(tf), 0.1)])
+        axs = (axs0, axs1, axs2)
+        for ax in axs:
+            ax.set(ylabel='Voltage[V]', xlabel='RC[s]')
+
+        fig.suptitle(f'When y(t=0) = {startt}')
+
+        for ax in axs:
+            ax.label_outer()
         plt.savefig(f'./q6_6_t{startt}.png')
         plt.clf()
+        print(f'when y(t=o) is {startt}')
+        print(max(data[0][1]))
+        print(max(data[1][1]))
+        print(max(data[2][1]))
+        print(scipy.signal.correlate(data[0][1], [f(0, t, 0.5) for t in arange(0, int(tf), 0.1)])[data[0][1].index(max(data[0][1]))] % (2 * pi))
+        print(scipy.signal.correlate(data[1][1], [f(0, t, 0.5) for t in arange(0, int(tf), 0.1)])[data[1][1].index(max(data[1][1]))] % 2 * pi)
+        print(scipy.signal.correlate(data[2][1], [f(0, t, 0.5) for t in arange(0, int(tf), 0.1)])[data[2][1].index(max(data[2][1]))] % 2 * pi)
